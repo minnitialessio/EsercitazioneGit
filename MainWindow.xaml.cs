0d03a2f6dea7da1fba6fcd8c2811b750314ec650
Francesco Meloni <FRANCESCO.MELONI@local.ittcesena.it>
2021-10-29 09:24:49 +0200

    Aggiunta Interfaccia e Main

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,57 @@ namespace EsercitazioneGit
         public MainWindow()
         {
             InitializeComponent();
+            cd = new CD("Doom", "Nayt");
+
+            MessageBox.Show("Brani:\n" + cd);
+
+            lbl_durataTot.Content = cd.DurataTot;
+
+        }
+
+        CD cd;
+
+        private void btn_aggiungiBrano_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Brano brano = new Brano("Doom", "Nayt", 180);
+                cd.AggiungiBrano(brano);
+
+                lbx_brani.Items.Add(brano);
+
+                lbl_durataTot.Content = cd.DurataTot;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex.Message);
+            }
+        }
+
+        private void btn_tempoBranoMinoreDi_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {                
+                if (lbx_brani.SelectedItem == null) 
+                {
+                    throw new Exception("Seleziona un brano");
+                }
+
+                Brano brano = lbx_brani.SelectedItem as Brano;
+
+                if (brano.ShortSong(tbx_tempoBranoMinoreDi.Text) == true) 
+                {
+                    MessageBox.Show("La durata del brano è minore");
+                }
+                else
+                {
+                    MessageBox.Show("La durata del brano è maggiore");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex.Message);
+            }
         }
     }
 }
